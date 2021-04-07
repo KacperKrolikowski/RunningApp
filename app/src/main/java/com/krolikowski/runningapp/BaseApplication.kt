1ec48adf8d79f39b68215c3b5554faa66e2c3f66
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-04-07 12:38:57 +0200

    Dagger-Hilt

---
+++ app/src/main/java/com/krolikowski/runningapp/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.krolikowski.runningapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+
+}
---
+++ app/src/main/java/com/krolikowski/runningapp/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.krolikowski.runningapp.other
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
